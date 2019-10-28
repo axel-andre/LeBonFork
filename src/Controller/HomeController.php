f886fa3538f384e3ac58e69e3cbe1029359bda0a
Axel André <98.axel.andre@gmail.com>
2019-10-28 14:55:23 +0100

    ✨ Setting up the product page list

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,26 +1,23 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
 class HomeController extends AbstractController {
 
     /**
      * HomeController constructor.
      * @Route("/", name="home")
-     * @var Environment $twig
+     * @param ProductRepository $repository
+     * @return Response
      */
-    private $twig;
-
-    public function __construct(Environment $twig){
-        $this->twig = $twig;
-    }
-
-    public function index(): Response{
-        return $this->render('pages/home.html.twig');
+    public function index(ProductRepository $repository): Response{
+        $products = $repository->findAll();
+        dump($products);
+        return $this->render('pages/home.html.twig',['products'=> $products]);
     }
 }
 
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,16 +1,38 @@
 <?php
 
 namespace App\Controller;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController {
+    private $repository;
+
     /**
      * @Route("/products", name="products.index")
+     * @param ProductRepository $repository
      * @return Response
      */
-    public function index(): Response{
-        return $this->render('products/index.html.twig');
+
+    public function index(ProductRepository $repository): Response{
+        $products = $repository->findAll();
+        return $this->render('products/index.html.twig',[
+            'current_menu'=> 'products',
+            'products' => $products
+        ]);
+    }
+    public function create(){
+        // New product creation
+        $product = new Product();
+        $product->setTitle('iPhone 11');
+        $product->setDescription('Il fait 64 GB.');
+        $product->setPrice(1000);
+        // EntityManager
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+        return null;
     }
 }
